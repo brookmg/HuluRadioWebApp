4c085efda31d99ccbb782f39843a20e2a00729c2
Abel Tesfaye <abeltesfaye45@gmail.com>
2021-10-08 17:43:52 +0300

    add nav bar with darkmode support

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchAppBar from './components/SearchAppBar'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+import * as React from 'react';
+
+export const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
 
 function App() {
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <SearchAppBar />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
